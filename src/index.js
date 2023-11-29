52ecd759490b7b055a3e3d1c774409b81c144521
corinachistol <chistolcorina19@gmail.com>
2023-11-29 16:01:34 +0200

    full version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,152 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  )
+}
+
+function Header(){
+  // const style = {color:"red", fontSize: "48px", textTransform:"uppercase"}
+
+  return  (
+    <header className="header">
+      <h1 >Fast React Pizza Co.</h1>
+
+    </header>
+  )
+}
+
+function Menu() {
+  // const pizzas = []
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length
+
+  return (
+  <main className="menu">
+    <h2>OurMenu</h2>
+
+      
+    {numPizzas > 0 ? ( 
+      <>
+        <p>Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.</p>
+
+        <ul className='pizzas'>
+          {pizzas.map(pizza=>(
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul> 
+      </>
+    ) : (
+      <p>We are still working on the menu.Please come back later!</p>
+    ) }
+
+  </main>
+  )
+}
+
+function Pizza( {pizzaObj} ) {
+  console.log(pizzaObj)
+
+  // if(pizzaObj.soldOut) return null;
+
+  return (
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ""}`}>
+      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+      <div>
+        <h3>{pizzaObj.name}</h3>
+        <p>{pizzaObj.ingredients}</p>
+
+        <span>{pizzaObj.soldOut ? "SOLD OUT" :  pizzaObj.price}</span>
+      </div>
+    </li>
+  )
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHours = 10;
+  const closeHours = 23;
+  const isOpen = openHours <= hour && hour <= closeHours;
+  console.log(isOpen)
+  // if(openHours >= hour && hour <= closeHours) alert("We are currently open!!!"); else alert("Sorry, we are closed!!!")
+
+  // if(!isOpen)
+  //   return (
+  //     <p>We're happy to welcome you between {openHours}:00 and {closeHours}:00 </p>
+  //   )
+
+  return (
+    <footer className="footer">
+      {isOpen ? <Order closeHours={closeHours} openHours={openHours}/> : <p>We're happy to welcome you between {openHours}:00 and {closeHours}:00 </p> }</footer>
+  )
+}
+
+function Order({closeHours, openHours}) {
+  return (
+    <div className="order">
+      <p>
+        We are currently open from {openHours}:00 until {closeHours}:00. Come visit us or order online
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  )
+}
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +155,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
